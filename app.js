45370548f9ff1b9064cbb022b15f4ada8faf144e
Rajith Noojibail <rajith@Rajiths-MacBook-Pro.local>
2017-11-06 18:09:12 +0530

    Change

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    session.send("Hey there. You said: %s", session.message.text);
 });
